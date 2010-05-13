6602cbf86bf8feb8e9b6870dfa221a67fa66b883
riedelcastro <sebastian.riedel@gmail.com>
2010-05-13 02:39:51 -0400

    fixed some pom and sbt issues

--- fast-froward-examples/project/build/FastForwardExamples.scala
+++ fast-froward-examples/project/build/FastForwardExamples.scala
@@ -4,4 +4,6 @@ class FastFrowardExamples(info: ProjectInfo) extends DefaultProject(info)
 {
   lazy val hi = task { println("Peace"); None }
 
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.0"
+  val junit = "junit" % "junit" % "4.4"
 }
